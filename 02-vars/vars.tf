97796ca3557f9a4c2e8daa4c0bbdd3dd93def83a
vravru135 <vravru135@gmail.com>
2024-03-02 14:48:01 +0530

    Adding shell Topics

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -8,20 +8,20 @@ output "sample1" {
 
 variable "fruits" {
   default = ["apple", "banana", "orange"]
- }
+}
 
 output "fruits_first_fruit" {
   value = var.fruits[0]
- }
+}
 
  variable "fruits_with_quantity" {
    default = {
-     apple - 100
-     banana= 200
-     orange= 50
+     apple = 100
+     banana = 200
+     orange = 50
    }
- }
+}
 
  output "fruits_apple_quantity" {
    value = var.fruits_with_quantity["apple"]
- }
+}
