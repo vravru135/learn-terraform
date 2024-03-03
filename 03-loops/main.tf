40baa6e401f9dc23fc91daa355aac06626dd21e0
vravru135 <vravru135@gmail.com>
2024-03-03 13:02:23 +0530

    Adding shell Topics

--- 03-loops/main.tf
+++ 03-loops/main.tf
@@ -12,3 +12,35 @@ variable "instances" {
 resource "null_resource" "sample" {
   count = length(var.instances)
 }
+
+variable "instances1" {
+   default = {
+     frontend = {
+       name = "frontend"
+       instance_type = "t3.micro"
+    }
+     backend = {
+       name = "backend"
+       instance_type = "t3.micro"
+    }
+     mysql = {
+       name = "frontend"
+       instance_type = "t3.micro"
+    }
+  }
+}
+
+  resource "null_resource" "sample1" {
+    for_each = var.instances1
+  }
+
+
+
+
+
+
+
+
+
+
+
