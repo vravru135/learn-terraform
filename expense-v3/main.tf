95ed698bcf153386afcdeb75e031394d00905ff1
vravru135 <vravru135@gmail.com>
2024-03-03 14:08:14 +0530

    Adding shell Topics

--- expense-v3/main.tf
+++ expense-v3/main.tf
@@ -8,13 +8,16 @@ resource "aws_instance" "instances" {
    }
 }
 
-#resource "aws_route53_record" "frontend" {
-#  zone_id = var.zone_id
-#  name    = "frontend-${var.env}"
-#  type    = "A"
-#  ttl     = 30
-#  records = [aws_instance.frontend.private_ip]
+resource "aws_route53_record" "frontend" {
+  for_each = var.components
+  zone_id = var.zone_id
+  name    = "${each.value["name"]}-${var.env}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances[each.value["name"]].private_ip]
+}
+
+
+#output "instances" {
+#  value = aws_instance.instances
 #}
-
-
-
