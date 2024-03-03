7919a1a229b5c57c5f5ab1c5bf595436b338627b
vravru135 <vravru135@gmail.com>
2024-03-03 10:17:48 +0530

    Adding shell Topics

---
+++ expense-v2/dev.tfvars
@@ -0,0 +1,5 @@
+env = "dev"
+ami = "ami-0f3c7d07486cad139"
+instance_type = "t3.micro"
+sg_id = ["sg-0158e4b6b7f7e323d"]
+zone_id = "Z069045511YTPWJUCXRCN"
--- expense-v2/main.tf
+++ expense-v2/main.tf
@@ -1,49 +1,49 @@
 resource "aws_instance" "frontend" {
-   ami = "ami-0f3c7d07486cad139"
-   instance_type = "t3.micro"
-   vpc_security_group_ids = ["sg-0158e4b6b7f7e323d"]
+   ami = var.ami
+   instance_type = var.instance_type
+   vpc_security_group_ids = var.sg_id
    tags = {
-     Name = "frontend"
+     Name = "frontend-${var.env}"
    }
 }
 
 resource "aws_route53_record" "frontend" {
-  zone_id = "Z069045511YTPWJUCXRCN"
-  name    = "frontend-dev"
+  zone_id = var.zone_id
+  name    = "frontend-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.frontend.private_ip]
 }
 
 resource "aws_instance" "backend" {
-   ami = "ami-0f3c7d07486cad139"
-   instance_type = "t3.micro"
-   vpc_security_group_ids = ["sg-0158e4b6b7f7e323d"]
+   ami = var.ami
+   instance_type = var.instance_type
+   vpc_security_group_ids = var.sg_id
    tags = {
      Name = "backend"
    }
 }
 
 resource "aws_route53_record" "backend" {
-  zone_id = "Z069045511YTPWJUCXRCN"
-  name    = "backend-dev"
+  zone_id = var.zone_id
+  name    = "backend-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.backend.private_ip]
 }
 
 resource "aws_instance" "mysql" {
-   ami = "ami-0f3c7d07486cad139"
-   instance_type = "t3.micro"
-   vpc_security_group_ids = ["sg-0158e4b6b7f7e323d"]
+   ami = var.ami
+   instance_type = var.instance_type"
+   vpc_security_group_ids = var.sg_id
    tags = {
      Name = "mysql"
    }
 }
 
 resource "aws_route53_record" "mysql" {
-  zone_id = "Z069045511YTPWJUCXRCN"
-  name    = "mysql-dev"
+  zone_id = var.zone_id
+  name    = "mysql-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
