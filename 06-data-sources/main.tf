1f24a8c61921bff4868ac4536056b46029acefd2
vravru135 <vravru135@gmail.com>
2024-03-15 08:55:37 +0530

    Adding shell Topics

--- 06-data-sources/main.tf
+++ 06-data-sources/main.tf
@@ -8,10 +8,10 @@
 
 data "aws_instance" "instance" {
   filter {
-   name   = "tag:name"
+   name   = "tag:Name"
    values =  ["workstation"]
-  }
  }
+}
 
  output "instance" {
    value = "Instance ID - ${data.aws_instance.instance.id} | Public IP - ${data.aws_instance.instance.public_ip}"
