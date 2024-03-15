cb891e498ad1fc9ef2ca901bb4cf84addd504259
vravru135 <vravru135@gmail.com>
2024-03-15 08:49:04 +0530

    Adding shell Topics

--- 06-data-sources/main.tf
+++ 06-data-sources/main.tf
@@ -1,7 +1,18 @@
-data "aws_ssm_parameter" "user" {
-  name = "dev.rds.username"
-}
+#data "aws_ssm_parameter" "user" {
+ # name = "dev.rds.username"
+#}
 
-output "username" {
-  value = data.aws_ssm_parameter.user.value
-}
+#output "username" {
+ # value = data.aws_ssm_parameter.user.value
+#}
+
+data "aws_instance" "instance" {
+  filter {
+   name   = "tag:name"
+   values =  ["workstation"]
+  }
+ }
+
+ output "instance" {
+   value = "Instance ID - ${data.aws_instance.instance.id} | Public IP - ${data.aws_instance.public_ip}"
+ }
